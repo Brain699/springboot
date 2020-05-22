596018c13f6e448f1a5c1651ccafd4c2bfc0e94b
minxiaoxian <tzmxx_great@126.com>
2020-05-22 09:50:27 +0800

    test

---
+++ src/test/java/com/people/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.people.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
